7a8f01764c06981250c6dd91c9e853c3f7904434
prince <pk9939952@gmail.com>
2026-08-02 23:36:16 +0530

    made a 2D rectangle generator

---
+++ NestedLoop.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   int rows;
+   int columns;
+   char symbol;
+   
+   cout << "enter the number of rows; ";
+   cin >> rows;
+
+   cout << "enter the number of colums: ";
+   cin >> columns;
+
+   cout << "enter the symbol: ";
+   cin >> symbol;
+
+   for(int i = 1; i <= rows; i++) 
+   {
+    for( int j=1; j <=columns; j++)
+    {
+        cout << symbol;
+    }
+    cout << endl;
+
+   }
+   
+
+
+}
+
+
+/* 
+.....loop (condition)
+    {
+        loop (codition)
+        {
+            loop (condition)
+            {
+                //code
+            }
+        }
+    }
+
+*/
