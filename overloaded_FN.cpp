f48a184ac576d85910a60353522c1cb991a644d7
prince <pk9939952@gmail.com>
2026-08-10 23:37:25 +0530

    feat: add makeroti functions with method overloadingImplements two versions of the makeroti function to support different use cases. The compiler distinguishes between them using their function signatures

--- overloaded_FN.cpp
+++ overloaded_FN.cpp
@@ -3,10 +3,14 @@
 // Function prototypes for function overloading
 void makeroti();
 void makeroti(std::string toppings1);
+void makeroti(std::string toppings1, std::string toping2);
 
 int main() {
+    
     //? Calls the overloaded version with a string argument
+
     makeroti(); // you can pass the toppings string to execute the "void makeroti(std::string topping1)"
+       //? or can pass the topping using the "void makeroti(std::string topping1, std::string topping2)"
     return 0;
 }
 
@@ -18,4 +22,7 @@ void makeroti() {
 //? Overloaded implementation: generates a customized roti based on the topping
 void makeroti(std::string topping1) {
     std::cout << "here is your " << topping1 << " roti \n"; 
+}
+void makeroti(std::string topping1, std::string topping2) {
+    std::cout << "here is your " << topping1 << "and " << topping2 <<" roti \n"; 
 }
