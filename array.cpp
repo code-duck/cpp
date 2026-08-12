2619a78f184c93c738512bdc9dbf154fa61c04d7
prince <pk9939952@gmail.com>
2026-08-12 19:35:55 +0530

    applied [ for each loop ] on an array

--- array.cpp
+++ array.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-int main()
+int main_1()
 {
     int A[5] = {1, 1, 2, 3, 4};
     
@@ -8,8 +8,32 @@ int main()
     std::cout<<sizeof(A)<<std::endl;
     std::cout<<A[2]<<std::endl;
 
-
-
     return 0;
 
 }
+
+int main()
+{
+    int B[5] = {5,34,5,32,1};
+
+  /*for (range_declaration : range_expression) 
+  {
+    // Loop body
+  }*/
+
+    for (int x : B) // for each loop          
+    {
+        std::cout<< x << std::endl;
+    }
+}
+
+/*
+A range-based for loop (for-each loop) is a loop that automatically 
+   runs a block of code exactly once for every item inside a collection.
+
+   WHAT IT DOES:
+   1. It looks at your collection (like an array or vector).
+   2. It automatically pulls out the first item and drops it into your loop variable.
+   3. It runs your code using that variable.
+   4. It automatically moves to the next item and repeats until it hits the end.
+*/
