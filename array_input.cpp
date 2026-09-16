a922a38059b74f8688a1bb85c92d274c20065a01
prince <pk9939952@gmail.com>
2026-09-17 01:23:08 +0530

    used for loop and used for each loop to dispkay output

---
+++ array_input.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+
+int main()
+{
+    std::string foods[5];
+    int size = sizeof(foods)/sizeof(foods[0]);
+
+    for (int i = 0 ; i < 5 ; i++){
+        std::cout << "enter food you like: " << i + 1  << ":" << std::endl;
+        std::getline(std::cin, foods[i]);
+       
+    }
+
+   std::cout << "you like: " << '\n';
+
+   for (std::string food : foods ){
+    std::cout <<  food << '\n' ;
+   }
+}
